63f63d80828a9ab2f6a518b5265865bc9f5a28c5
Ben Woodfield <raser.13@hotmail.com>
2018-03-29 09:13:55 +0100

    Update pyGoogleSearch.py

--- pyGoogleSearch.py
+++ pyGoogleSearch.py
@@ -1,3 +1,6 @@
+# Ben Woodfield
+# Written in Python 2
+
 # This asks user for a word to search in Google
 # Then concatenates their search request string with the address string
 # the address has to be like this to allow it to read as a google search:
